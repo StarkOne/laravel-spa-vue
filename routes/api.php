8cca02e59a84487e1f30322a1ffbb861ba3a03fd
StarkOne <stark.pro40@gmail.com>
2020-05-09 14:41:04 +0700

    Resource

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::resource('bookables', 'Api\BookableController')->only(['index', 'show']);
+Route::get('bookables/{bookable}/availability', 'Api\BookableAvailabilityController')->name('bookable.availability.show');
